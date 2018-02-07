14390c2fbc2007426adfedfb3a36c5aab2781af5
achialandry <achialands@icloud.com>
2018-02-06 19:49:55 -0700

    d fix

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'jtabc'),
             'username' => env('DB_USERNAME', 'root_jtabc'),
-            'password' => env('DB_PASSWORD', ' 469816'),
+            'password' => env('DB_PASSWORD', ' newera2016'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
